10d4c3d12d1d7f07c01eeb3511640e18d6b7d09a
EllinFeh <elisonfelipe16@hotmail.com>
2023-11-03 18:47:20 -0300

    likes, subscribe and channel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './components/navbar/App';
+import Navbar from './components/navbar/Navbar';
+import Watch from './components/watch/Watch';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+<>
   <Navbar />
-  
+  <Watch />
+</>
 );
 
